00f708dc7ce18bf192b712e6a4d65b494760838d
JuanmaGaitan24 <juanmanuelgaitan@iesflorenciopintado.es>
2022-10-31 12:14:28 +0100

    Dame un Susto V3.5

--- app/src/main/java/com/example/dameunsusto/MainActivity.java
+++ app/src/main/java/com/example/dameunsusto/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -20,6 +21,32 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout listaSpinner, listaNsegundos, listaplay;
     EditText etCantSustos;
     Button btnCrearSustos;
+
+    int[] logos = new int[]{
+        R.drawable.alien, R.drawable.arbusto,
+        R.drawable.brokenglass, R.drawable.cadenas,
+        R.drawable.chainsaw, R.drawable.doorclosing,
+        R.drawable.evillaught, R.drawable.fallingwarning,
+        R.drawable.footsteps, R.drawable.guts,
+        R.drawable.heartbeat, R.drawable.knife,
+        R.drawable.knock, R.drawable.monstruo,
+        R.drawable.song, R.drawable.storm,
+        R.drawable.wind, R.drawable.wolf,
+        R.drawable.woodcreaks, R.drawable.zombie
+    };
+
+    String[] nombresSonidos = new String[]{
+        String.valueOf(R.string.alien), String.valueOf(R.string.arbusto),
+        String.valueOf(R.string.cristalroto), String.valueOf(R.string.cadenas),
+        String.valueOf(R.string.motosierra), String.valueOf(R.string.puertacerrandose),
+        String.valueOf(R.string.risamalevola), String.valueOf(R.string.mujercallendo),
+        String.valueOf(R.string.pasos), String.valueOf(R.string.tripas),
+        String.valueOf(R.string.latidos), String.valueOf(R.string.cuchillo),
+        String.valueOf(R.string.llamarpuerta), String.valueOf(R.string.rugidomonstruo),
+        String.valueOf(R.string.cancion), String.valueOf(R.string.tormenta),
+        String.valueOf(R.string.vientolluvia), String.valueOf(R.string.lovos),
+        String.valueOf(R.string.maderacruguiendo), String.valueOf(R.string.zombie)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,24 +70,29 @@ public class MainActivity extends AppCompatActivity {
                     for (int i = 0; i < numsustos; i++){
 
                         Spinner listaSonidos = new Spinner(MainActivity.this);
-                        TextView Nsegundos = new TextView(MainActivity.this);
+                        EditText Nsegundos = new EditText(MainActivity.this);
                         Button btnCuentaAtras = new Button(MainActivity.this);
 
-                        listaSonidos.setLayoutParams(new LinearLayout.LayoutParams(150, 70));
-                        listaSonidos.setPadding(20,40,0,40);
+                        listaSonidos.setLayoutParams(new LinearLayout.LayoutParams(400, 250));
+                        listaSonidos.setPadding(20,60,0,40);
 
-                        Nsegundos.setLayoutParams(new LinearLayout.LayoutParams(125, 70));
+                        Nsegundos.setLayoutParams(new LinearLayout.LayoutParams(350, 250));
                         Nsegundos.setTextColor(getResources().getColor(android.R.color.holo_orange_dark));
                         Nsegundos.setHint(getString(R.string.numerosegundos));
+                        Nsegundos.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
                         Nsegundos.setHintTextColor(getResources().getColor(android.R.color.holo_orange_light));
-                        Nsegundos.setPadding(30,40,0,40);
+                        Nsegundos.setPadding(30,60,0,40);
 
-                        btnCuentaAtras.setLayoutParams(new LinearLayout.LayoutParams(100, 70));
+                        btnCuentaAtras.setLayoutParams(new LinearLayout.LayoutParams(300, 250));
                         btnCuentaAtras.setBackground(getResources().getDrawable(R.drawable.play));
 
                         listaSpinner.addView(listaSonidos);
                         listaNsegundos.addView(Nsegundos);
                         listaplay.addView(btnCuentaAtras);
+
+                        ArrayAdapter<String> adaptador;
+                        adaptador = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, nombresSonidos);
+
 
                     }
 
